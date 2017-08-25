c3a6f764fe344ebb546eaa9ff6f7150343fc037a
爱人 <csecong@126.com>
2017-08-25 11:22:59 +0800

    常用方法
    一些用到的方法

---
+++ codelist/Common.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codelist
+{
+    public class Common
+    {
+        /// <summary>
+        /// 获取Get/Post/Cookies/ServerVariables
+        /// </summary>
+        /// <param name="k"></param>
+        /// <param name="var"></param>
+        /// <returns></returns>
+        public static string gpc(string k, string var = "P")
+        {
+            string return_str = "";
+
+            switch (var)
+            {
+                case "G":
+                    return_str = System.Web.HttpContext.Current.Request.QueryString[k] == null ? "" : System.Web.HttpContext.Current.Request.QueryString[k].ToString();
+                    break;
+                case "P":
+                    return_str = System.Web.HttpContext.Current.Request.Form[k] == null ? "" : System.Web.HttpContext.Current.Request.Form[k].ToString();
+                    break;
+                case "S":
+                    return_str = System.Web.HttpContext.Current.Request.ServerVariables[k] == null ? "" : System.Web.HttpContext.Current.Request.ServerVariables[k].ToString();
+                    break;
+                case "C":
+                    return_str = System.Web.HttpContext.Current.Request.Cookies[k] == null ? "" : System.Web.HttpContext.Current.Request.Cookies[k].ToString();
+                    break;
+                case "R":
+                    return_str = System.Web.HttpContext.Current.Request[k] == null ? "" : System.Web.HttpContext.Current.Request[k].ToString();
+                    break;
+            }
+            return return_str;
+        }
+
+        /// <summary>
+        /// MD5加密
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+        public static string Encrypt(string input, Encoding encoding = null)
+        {
+            if (encoding == null)
+            {
+                encoding = Encoding.UTF8;
+            }
+            //using (MD5CryptoServiceProvider md5Hash = new MD5CryptoServiceProvider())
+            using (System.Security.Cryptography.MD5 md5Hash = System.Security.Cryptography.MD5.Create())
+            {
+                // Convert the input string to a byte array and compute the hash.
+                byte[] data = md5Hash.ComputeHash(encoding.GetBytes(input));
+
+                // Create a new Stringbuilder to collect the bytes
+                // and create a string.
+                StringBuilder sBuilder = new StringBuilder();
+
+                // Loop through each byte of the hashed data
+                // and format each one as a hexadecimal string.
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+
+                // Return the hexadecimal string.
+                return sBuilder.ToString();
+            }
+        }
+    }
+}
---
+++ codelist/open_baidu_zhitou.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codelist
+{
+    class open_baidu_zhitou
+    {
+        /// <summary>
+        /// 百度直投简历解密接口C# 基于base64_decode的中文解密算法
+        /// </summary>
+        /// <param name="data_str">待解密字符串res</param>
+        /// <param name="key">密钥key</param>
+        /// <returns>解密后字符串</returns>
+        public static string bd_res_encode(string data_str, string key)
+        {
+            //MD5
+            key = Common.Encrypt(key);
+
+            int l = key.Length;
+            int x = 0;
+            //base64
+            byte[] c = Convert.FromBase64String(data_str);
+            int len = c.Length;
+
+            string c_str = "";
+
+            for (int i = 0; i < len; ++i)
+            {
+                if (x == l)
+                {
+                    x = 0;
+                }
+                c_str += key[x];
+                ++x;
+            }
+            string res_str = "";
+
+            for (int i = 0; i < len; ++i)
+            {
+                int c1 = (int)c[i];
+                int c2 = (int)c_str.Substring(i, 1)[0];
+
+                if (c1 < c2)
+                {
+                    res_str += Convert.ToChar(c1 + 256 - c2);
+                }
+                else
+                {
+                    res_str += Convert.ToChar(c1 - c2);
+                }
+            }
+
+            return res_str;
+        }
+    }
+}
