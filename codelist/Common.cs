8408bdfee3980176508b9236b3d8cc7a7b1f7ba7
爱人 <csecong@126.com>
2017-08-25 11:46:18 +0800

    常用方法

--- codelist/Common.cs
+++ codelist/Common.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web;
 
@@ -73,6 +74,83 @@ namespace codelist
                 // Return the hexadecimal string.
                 return sBuilder.ToString();
             }
+        }
+
+        /// <summary>
+        /// 验证国内手机号码
+        /// </summary>
+        /// <param name="tel"></param>
+        /// <returns></returns>
+        public static bool regex_mobile(string tel)
+        {
+            //电信手机号正则
+            string dianxin = @"^1[3578][01379]\d{8}$";
+            Regex dReg = new Regex(dianxin);
+            //联通手机号正则
+            string liantong = @"^1[34578][01256]\d{8}$";
+            Regex tReg = new Regex(liantong);
+            //移动手机号正则
+            string yidong = @"^(134[012345678]\d{7}|1[34578][012356789]\d{8})$";
+            Regex yReg = new Regex(yidong);
+
+            if (dReg.IsMatch(tel) || tReg.IsMatch(tel) || yReg.IsMatch(tel))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        /// <summary>
+        /// 过滤html
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string remove_html(string str)
+        {
+            string regexstr = @"<[^>]*>";
+            return System.Text.RegularExpressions.Regex.Replace(str, regexstr, string.Empty, System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+        }
+        
+        /// <summary>
+        /// 测试是否复杂密码
+        /// </summary>
+        /// <param name="pass_str"></param>
+        /// <returns></returns>
+        public static bool pass_strong(string pass_str)
+        {
+            var regex = new Regex(@"
+(?=.*[0-9])                     #必须包含数字
+(?=.*[a-zA-Z])                  #必须包含小写或大写字母
+(?=([\x21-\x7e]+)[^a-zA-Z0-9])  #必须包含特殊符号
+.{8,30}                         #至少8个字符，最多30个字符
+", RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace);
+
+            if (regex.IsMatch(pass_str))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+
+        /// <summary>
+        /// 是否数字
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsNumeric(string str)
+        {
+            System.Text.RegularExpressions.Regex reg1 = new System.Text.RegularExpressions.Regex(@"^[0-9]\d*$");
+            return reg1.IsMatch(str);
         }
     }
 }
